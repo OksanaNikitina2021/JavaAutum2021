2ecc8670cc3f591036d714b4290b0b477b8d6016
Oksana Nikitina <oksdnd@gmail.com>
2021-10-13 16:19:19 +0300

    Third day at bootcamp

---
+++ SEF - Participants Workspace/SEF/src/sef/module7/sample/StrategyImplFour.java
@@ -0,0 +1,9 @@
+package sef.module7.sample;
+
+public class StrategyImplFour implements Strategy{
+
+    public void execute(){
+        System.out.println("Queen moves!");
+    }
+
+}
---
+++ SEF - Participants Workspace/SEF/src/sef/module7/sample/StrategyImplThree.java
@@ -0,0 +1,9 @@
+package sef.module7.sample;
+
+public class StrategyImplThree implements Strategy{
+
+    public void execute(){
+        System.out.println("King moves!");
+    }
+
+}
