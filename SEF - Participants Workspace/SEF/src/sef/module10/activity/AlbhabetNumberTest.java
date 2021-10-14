79543eb37e87cf0289358b6fadd6a82ad3e74831
Oksana Nikitina <oksdnd@gmail.com>
2021-10-14 17:07:48 +0300

    Fourth day at bootcamp

--- SEF - Participants Workspace/SEF/src/sef/module10/activity/AlbhabetNumberTest.java
+++ SEF - Participants Workspace/SEF/src/sef/module10/activity/AlbhabetNumberTest.java
@@ -5,76 +5,93 @@ package sef.module10.activity;
 //against those numbers 
 
 public class AlbhabetNumberTest {
-	public static void main(String[] args)
-	{
-		Generator obj = new Generator();
-		
-		//1 - Create objects of both the thread classes and pass obj as a parameter to them 
-		
-		
-		//2 - Start both the threads
-		
-		}
-	}
+    public static void main(String[] args) {
+        Generator obj = new Generator();
 
-class NumGenerator extends Thread
-{
-	public Generator obj;
-	public NumGenerator(Generator obj)
-	{
-		this.obj = obj;
-	}
-	public void run()
-	{
-		//3 - Run a loop from 1-26 (say i is the variable) and
-		//call printNumber passing i as its parameter 
-	}
-}  
+        //1 - Create objects of both the thread classes and pass obj as a parameter to them
+        NumGenerator numGenerator = new NumGenerator(obj);
+        AlphaGenerator alphaGenerator = new AlphaGenerator(obj);
 
-class AlphaGenerator extends Thread
-{
-	public Generator obj;
-	public AlphaGenerator(Generator obj)
-	{
-		this.obj = obj;
-	}
-	public void run()
-	{
-		
-		//4 - Run a loop from A-Z (say c is the variable) and
-		//call printAlbhabet passing c as its parameter
-	
-	}
+        //2 - Start both the threads
+        numGenerator.start();
+        alphaGenerator.start();
+    }
+}
+
+class NumGenerator extends Thread {
+    public Generator obj;
+
+    public NumGenerator(Generator obj) {
+        this.obj = obj;
+    }
+
+    public void run() {
+        //3 - Run a loop from 1-26 (say i is the variable) and
+        //call printNumber passing i as its parameter
+        for (int i = 1; i <= 26; i++) {
+            try {
+                obj.printNumber(i);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
+
+class AlphaGenerator extends Thread {
+    public Generator obj;
+
+    public AlphaGenerator(Generator obj) {
+        this.obj = obj;
+    }
+
+    public void run() {
+
+        //4 - Run a loop from A-Z (say c is the variable) and
+        //call printAlbhabet passing c as its parameter
+        char c;
+        for (c = 'A'; c <= 'Z'; c++) {
+            try {
+                obj.printAlphabet(c);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
 
 class Generator {
-	boolean numPrinted = false;
-	public synchronized void printNumber(int number)
-	{
-		//5 - Check if numPrinted is true, then call wait()
-		
+    boolean numPrinted = false;
 
-		//6 - print the number here
+    public synchronized void printNumber(int number) throws InterruptedException {
+        //5 - Check if numPrinted is true, then call wait()
+        if (numPrinted) {
+            wait(2000);
+        }
 
-		
-		//7 - numPrinted should be assigned true here
+        //6 - print the number here
+        System.out.print(number + " ");
 
-		
-		//8 - Notify the waiting thread
+        //7 - numPrinted should be assigned true here
+        numPrinted = true;
 
-	}
-	public synchronized void printAlphabet(char alphabet)
-	{
-		//9 - Check if numPrinted is false, then call wait()
-		
+        //8 - Notify the waiting thread
+        notify();
+    }
 
-		//10 - print the alphabet here
+    public synchronized void printAlphabet(char alphabet) throws InterruptedException {
+        //9 - Check if numPrinted is false, then call wait()
+        if (!numPrinted) {
+            wait(2000);
+        }
 
-		
-		//11 - numPrinted should be assigned true here
+        //10 - print the alphabet here
+        System.out.println(alphabet);
 
+        //11 - numPrinted should be assigned true here
+        numPrinted = false;
 
-		//12 - Notify the waiting thread
-
-	}
+        //12 - Notify the waiting thread
+        notify();
+    }
 } 
--- SEF - Participants Workspace/SEF/src/sef/module17/sample/Product.java
+++ SEF - Participants Workspace/SEF/src/sef/module17/sample/Product.java
@@ -1,33 +1,34 @@
 package sef.module17.sample;
 // Complete Code
+
 import java.io.Serializable;
 
 public class Product implements Serializable {
-	
-	private String description;
-	private Double price;
-	 
-	public String getDescription() {
-		return description;
-	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    private String description;
+    private Double price;
 
-	public Double getPrice() {
-		return price;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setPrice(Double price) {
-		this.price = price;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String toString() {
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String toString() {
         StringBuffer buffer = new StringBuffer();
         buffer.append("Description: " + description + ";");
         buffer.append("Price: " + price);
         return buffer.toString();
     }
-	
+
 }
